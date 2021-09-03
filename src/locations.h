40e98ba22a4c30e79e6022c079439dd670f3d714
bitconnector <luk-krat@web.de>
2021-09-03 19:16:27 +0200

    fixed bug in example-config

--- src/locations.h
+++ src/locations.h
@@ -7,8 +7,8 @@ struct Geofence
     u_int radius;
 };
 
-static Geofence bielefeld[] = {
-    {52.01910, 8.53100, 10000}, //Bielefeld
-    {52.51704, 13.38886, 15000} //Berlin
-    {48.77845, 9.18001, 5000}   //Stuttgart
+static Geofence geofence[] = {
+    {52.01910, 8.53100, 10000},  //Bielefeld
+    {52.51704, 13.38886, 15000}, //Berlin
+    {48.77845, 9.18001, 5000}    //Stuttgart
 };
