318a15bce691f6343e3c7306ec70cc8726eea6a1
bitconnector <luk-krat@web.de>
2021-12-13 00:13:26 +0100

    switched from LMIC to LittleWan

--- src/lorawan.cpp
+++ src/lorawan.cpp
@@ -1,127 +1,94 @@
 #include "lorawan.hpp"
 
-RTC_DATA_ATTR lmic_t RTC_LMIC;
+unsigned char Buffer[235];
+RTC_DATA_ATTR LoraWANmessage message = LoraWANmessage(Buffer);
 
-osjob_t sendjob;
-
-const lmic_pinmap lmic_pins = {
-    .nss = LoRa_CS,
-    .rxtx = LMIC_UNUSED_PIN,
-    .rst = LoRa_RST,
-    .dio = {LoRa_DIO0, LoRa_DIO1, LoRa_DIO2},
-};
-
-#ifdef USE_OTAA
-void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
-void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
-void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
-#else
-void os_getArtEui(u1_t *buf) {}
-void os_getDevEui(u1_t *buf) {}
-void os_getDevKey(u1_t *buf) {}
-#endif
-
-
-uint8_t *data;
-uint8_t size;
-uint8_t port;
-bool confirm;
 bool has_send = false;
 
 void startup_lorawan()
 {
-    os_init();
-    LMIC_reset();
+    LoRa.setPins(LoRa_CS, LoRa_RST, LoRa_DIO0);
+    delay(100);
+    LoRa.begin(868100000);
+    LoRa.setPreambleLength(8);
+    LoRa.setSyncWord(0x34);
+    LoRa.setCodingRate4(5);
+    LoRa.setSpreadingFactor(7);
+    LoRa.setSignalBandwidth(125E3);
 
-    if (RTC_LMIC.seqnoUp != 0)
+    has_send = 0;
+
+    if (message.frameCounterUp == 0)
     {
-        LMIC = RTC_LMIC;
-    }
-    else
-    {
-#ifdef PROGMEM
-#ifndef USE_OTAA
-        // On AVR, these values are stored in flash and only copied to RAM
-        // once. Copy them to a temporary buffer here, LMIC_setSession will
-        // copy them into a buffer of its own again.
-        uint8_t appskey[sizeof(APPSKEY)];
-        uint8_t nwkskey[sizeof(NWKSKEY)];
-        memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
-        memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
-        LMIC_setSession(0x1, DEVADDR, nwkskey, appskey);
-#endif
-#else
-        // If not running an AVR with PROGMEM, just use the arrays directly
-        LMIC_setSession(0x1, DEVADDR, NWKSKEY, APPSKEY);
-#endif
-        // Set up the channels used by the Things Network, which corresponds
-        // to the defaults of most gateways. Without this, only three base
-        // channels from the LoRaWAN specification are used, which certainly
-        // works, so it is good for debugging, but can overload those
-        // frequencies, so be sure to configure the full frequency range of
-        // your network here (unless your network autoconfigures them).
-        // Setting up channels should happen after LMIC_setSession, as that
-        // configures the minimal channel set.
-        // NA-US channels 0-71 are configured automatically
-        LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI); // g-band
-        LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
-        LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK, DR_FSK), BAND_MILLI);   // g2-band
+        LoraWANactivation otaa = LoraWANactivation(&message);
+        otaa.setDevNonce((uint16_t)random(70225));
+        otaa.setDevEUI(DEVEUI);
+        otaa.setJoinEUI(APPEUI);
+        otaa.setAppKey(APPKEY);
 
-        // disable channels (only use channel 0 - 868.1 MHz - for my single channel gateway!!!)
-        // for (int channel = 1; channel <= 8; channel++) {
-        //   LMIC_disableChannel(channel);
-        // }
+        Serial.print("Joining");
+        bool joined = 0;
+        while (!joined)
+        {
+            LoRa.begin(868100000);
+            LoRa.enableCrc();
+            LoRa.disableInvertIQ();
 
-        // Disable link check validation
-        LMIC_setLinkCheckMode(0);
+            esp_random();
+            otaa.setDevNonce((uint16_t)random(256 * 256));
+            otaa.joinmsg();
+            LoRa.beginPacket(); // start packet
+            LoRa.write(message.data, message.dataLen);
+            LoRa.endPacket(); // finish packet and send it
+            unsigned long txTime = millis();
 
-        // TTN uses SF9 for its RX2 window.
-        LMIC.dn2Dr = DR_SF9;
-
-        // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-        LMIC_setDrTxpow(DR_SF7, 14);
+            //RX1
+            LoRa.disableCrc();
+            LoRa.enableInvertIQ();
+            LoRa.receive();
+            while (txTime + 10000 > millis() && !joined)
+            {
+                if (LoRa.parsePacket())
+                {
+                    message.dataLen = 0;
+                    while (LoRa.available())
+                        message.data[message.dataLen++] = LoRa.read();
+                    joined = otaa.checkJoin((char *)message.data, message.dataLen);
+                    printPackage((char *)message.data, message.dataLen, 0);
+                }
+            }
+            Serial.print(".");
+        }
+        Serial.println("success!");
     }
 }
 
 void lorawan_loop()
 {
-    os_runloop_once();
+    delay(0);
 }
 
 void lorawan_send(uint8_t _port, uint8_t *_data, uint8_t _size, bool _confirm)
 {
-    data = _data;
-    size = _size;
-    port = _port;
-    confirm = _confirm;
-    do_send(&sendjob);
-}
+    LoRa.begin(868100000);
+    LoRa.setPreambleLength(8);
+    LoRa.setSyncWord(0x34);
+    LoRa.enableCrc();
+    LoRa.disableInvertIQ();
+    LoRa.setCodingRate4(5);
+    LoRa.setSpreadingFactor(7);
+    LoRa.setSignalBandwidth(125E3);
 
-void do_send(osjob_t *j)
-{
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND)
-    {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-    }
-    else
-    {
-        Serial.print("sending port: ");
-        Serial.print(port);
-        Serial.print(" data: ");
-        for (int i = 0; i < size; i++)
-            Serial.print(data[i], HEX);
-        Serial.print("  ");
-        Serial.println(size);
-        LMIC_setTxData2(port, data, size, confirm);
-        Serial.println(F("Packet queued"));
-    }
+    // char payload[100];
+    // sprintf(payload, "r=%02x", (int)random(256));
+    // Serial.printf("Sending: %s\n", payload);
+
+    message.uplink((char *)_data, _size, _port, _confirm);
+
+    LoRa.beginPacket(); // start packet
+    LoRa.write(message.data, message.dataLen);
+    LoRa.endPacket(); // finish packet and send it
+    has_send = 1;
 }
 
 void printHex2(unsigned v)
@@ -132,100 +99,6 @@ void printHex2(unsigned v)
     Serial.print(v, HEX);
 }
 
-void onEvent(ev_t ev)
-{
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch (ev)
-    {
-    case EV_SCAN_TIMEOUT:
-        Serial.println(F("EV_SCAN_TIMEOUT"));
-        break;
-    case EV_BEACON_FOUND:
-        Serial.println(F("EV_BEACON_FOUND"));
-        break;
-    case EV_BEACON_MISSED:
-        Serial.println(F("EV_BEACON_MISSED"));
-        break;
-    case EV_BEACON_TRACKED:
-        Serial.println(F("EV_BEACON_TRACKED"));
-        break;
-    case EV_JOINING:
-        Serial.println(F("EV_JOINING"));
-        break;
-    case EV_JOINED:
-        Serial.println(F("EV_JOINED"));
-        {
-            u4_t netid = 0;
-            devaddr_t devaddr = 0;
-            u1_t nwkKey[16];
-            u1_t artKey[16];
-            LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
-            Serial.print("netid: ");
-            Serial.println(netid, DEC);
-            Serial.print("devaddr: ");
-            Serial.println(devaddr, HEX);
-            Serial.print("AppSKey: ");
-            for (size_t i = 0; i < sizeof(artKey); ++i)
-            {
-                if (i != 0)
-                    Serial.print("-");
-                printHex2(artKey[i]);
-            }
-            Serial.println("");
-            Serial.print("NwkSKey: ");
-            for (size_t i = 0; i < sizeof(nwkKey); ++i)
-            {
-                if (i != 0)
-                    Serial.print("-");
-                printHex2(nwkKey[i]);
-            }
-            Serial.println();
-        }
-        // Disable link check validation (automatically enabled
-        // during join, but not supported by TTN at this time).
-        LMIC_setLinkCheckMode(0);
-        break;
-    case EV_RFU1:
-        Serial.println(F("EV_RFU1"));
-        break;
-    case EV_TXCOMPLETE:
-        Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-        if (LMIC.txrxFlags & TXRX_ACK)
-        {
-            Serial.println(F("Received ack"));
-        }
-        if (LMIC.dataLen)
-        {
-            Serial.print(F("Received "));
-            Serial.print(LMIC.dataLen);
-            Serial.println(F(" bytes of payload"));
-        }
-        has_send = true;
-        break;
-    case EV_LOST_TSYNC:
-        Serial.println(F("EV_LOST_TSYNC"));
-        break;
-    case EV_RESET:
-        Serial.println(F("EV_RESET"));
-        break;
-    case EV_RXCOMPLETE:
-        // data received in ping slot
-        Serial.println(F("EV_RXCOMPLETE"));
-        break;
-    case EV_LINK_DEAD:
-        Serial.println(F("EV_LINK_DEAD"));
-        break;
-    case EV_LINK_ALIVE:
-        Serial.println(F("EV_LINK_ALIVE"));
-        break;
-    default:
-        Serial.print(F("Unknown event: "));
-        Serial.println(ev);
-        break;
-    }
-}
-
 bool lorawan_has_send()
 {
     return has_send;
@@ -233,30 +106,34 @@ bool lorawan_has_send()
 
 void lorawan_sleep(unsigned long sleep_time)
 {
-    RTC_LMIC = LMIC;
-
-    unsigned long time = millis() + sleep_time;
-    time = (time / 1000) * OSTICKS_PER_SEC;
-
-    // EU Like Bands
-#if defined(CFG_eu868)
-    Serial.println(F("Reset CFG_LMIC_EU_like band avail"));
-    for (int i = 0; i < MAX_BANDS; i++)
-    {
-        ostime_t correctedAvail = RTC_LMIC.bands[i].avail - time;
-        if (correctedAvail < 0)
-        {
-            correctedAvail = 0;
-        }
-        RTC_LMIC.bands[i].avail = correctedAvail;
-    }
-
-    RTC_LMIC.globalDutyAvail = RTC_LMIC.globalDutyAvail - time;
-    if (RTC_LMIC.globalDutyAvail < 0)
-    {
-        RTC_LMIC.globalDutyAvail = 0;
-    }
-#else
     Serial.println(F("No DutyCycle recalculation function!"));
-#endif
+    LoRa.sleep();
+}
+
+void printPackage(char *data, uint16_t size, bool structure)
+{
+    Serial.print("+ Package:");
+    for (int i = 0; i < size; i++)
+        Serial.printf(" %02x", data[i]);
+    if (structure)
+    {
+        Serial.print("\n          |hd|   addr    |Ct| FCnt|");
+        if (size < 13)
+        {
+            Serial.println("    MIC    |");
+        }
+        else if (size == 13)
+        {
+            Serial.println("Pt|    MIC    |");
+        }
+        else
+        {
+            Serial.print("Pt|");
+            for (int i = 14; i < size; i++)
+                Serial.print("   ");
+            Serial.println("  |    MIC    |");
+        }
+    }
+    else
+        Serial.println();
 }
