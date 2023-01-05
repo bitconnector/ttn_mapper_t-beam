3826dc0945a0bee43e5456c7ec6519ac5075af67
Lukas <luk-krat@web.de>
2023-01-05 16:04:45 +0100

    fixed cubecell build error

--- src/cubecell.cpp
+++ src/cubecell.cpp
@@ -1,118 +1,16 @@
-/*
-good sources:
-https://github.com/JackGruber/ESP32-LMIC-DeepSleep-example
-https://randomnerdtutorials.com/esp32-deep-sleep-arduino-ide-wake-up-sources/
-*/
-#include <Arduino.h>
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
 
-#include "gps.hpp"
-#include "lorawan.hpp"
-#ifdef ESP32
-#include "power.hpp"
-#endif
+//#include "gps.hpp"
+//#include "lorawan.hpp"
 #include "config.hpp"
 
 unsigned int TX_INTERVAL = GPS_INTERVAL;
-SLEEP_VAR int wakeup_count = 0;
+int wakeup_count = 0;
 
 void sendLocation();
 void sendStatus(int state, int gps);
 
-#ifdef ESP32
-void setSleepTimer(int seconds);
-void enterSleep();
-
-void setup()
-{
-  wakeup_count++;
-  pinMode(LED, OUTPUT);
-  pinMode(ButtonPin, INPUT);
-  Serial.begin(115200);
-  digitalWrite(LED, LOW); // LED on
-  startup_axp();
-
-  Serial.println(axp.getBattVoltage());
-
-  esp_sleep_wakeup_cause_t wakeup_reason =
-      esp_sleep_get_wakeup_cause();
-  if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) // <-------------- timer
-  {
-    Serial.println(F("Wakeup caused by timer"));
-    int gpsStatus = getGPS();
-
-    if (gpsStatus == 0)
-      axp_gps(1); // give GPS more volt to get a fix
-    else
-      axp_gps(2); // turn down voltage for GPS to save energy
-
-    if (wakeup_count % STATUS_INTERVAL == 0)
-    {
-      Serial.println(F("periodically send state"));
-      sendStatus(4, gpsStatus);
-    }
-    if (gpsStatus == 1)
-      sendLocation();
-    setSleepTimer(TX_INTERVAL);
-  }
-  else if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0)
-  {
-    Serial.println(F("Wakeup caused by axp"));
-    axp_gps(1); // turn GPS on
-    uint8_t cause = axp_cause();
-    if (cause == 1) // <----------------------------- short press power
-    {
-      Serial.println(F("send status and location"));
-      int gpsStatus = getGPS();
-      sendStatus(1, gpsStatus);
-      if (gpsStatus == 1 || gpsStatus == 2)
-        sendLocation();
-      setSleepTimer(TX_INTERVAL);
-    }
-    else if (cause == 2) // <------------------------- long press power
-    {
-      Serial.print(F("entering deep sleep for infinity\n"));
-      axp_gps(0); // turn GPS off
-      sendStatus(3, 0);
-      digitalWrite(LED, HIGH); // turn the LED off
-      enterSleep();            // enter sleep without timer
-    }
-    else if (cause == 3) // <------------------------- battery critical
-    {
-      Serial.println(F("Battery low shutting down"));
-    }
-  }
-  else // <------------------------------------------------------ reset
-  {
-    Serial.println(F("Wakeup caused by reset"));
-    delay(3000);
-    setup_axp();
-    end_gps();
-    startup_lorawan();
-    sendStatus(2, 0);
-    setSleepTimer(TX_INTERVAL);
-  }
-
-  pinMode(LED, INPUT_PULLDOWN); // let the LED glim
-  enterSleep();
-}
-
-void loop()
-{
-}
-
-void setSleepTimer(int seconds)
-{
-  esp_sleep_enable_timer_wakeup(seconds * 1000000);
-}
-
-void enterSleep()
-{
-  lorawan_sleep();
-  axp_sleep();
-  Serial.flush();
-  esp_deep_sleep_start();
-}
-#else
 void setup()
 {
   pinMode(Vext, OUTPUT);
@@ -120,11 +18,11 @@ void setup()
   Serial.begin(115200);
   // digitalWrite(Vext, LOW);
 
-  lorawan_sleep();
+  //lorawan_sleep();
 
   Serial.println(F("Startup"));
   delay(3000);
-  startup_lorawan();
+  //startup_lorawan();
   // sendStatus(2, 0);
 }
 
@@ -138,7 +36,7 @@ void loop()
   if (digitalRead(ButtonPin)) // <-------------- timer
   {
     Serial.println(F("Wakeup caused by timer"));
-    int gpsStatus = getGPS();
+    int gpsStatus = 0;//getGPS();
 
     if (wakeup_count % STATUS_INTERVAL == 0)
     {
@@ -156,7 +54,7 @@ void loop()
     if (cause == 1) // <----------------------------- short press power
     {
       Serial.println(F("send status and location"));
-      int gpsStatus = getGPS();
+      int gpsStatus = 0;//getGPS();
       sendStatus(1, gpsStatus);
       if (gpsStatus == 1 || gpsStatus == 2)
         sendLocation();
@@ -181,22 +79,21 @@ uint8_t vbatt_bin(uint8_t *txBuffer, uint8_t offset)
   txBuffer[offset] = (getBatteryVoltage() / 10) - 250;
   return offset + 1;
 }
-#endif
 
 void sendLocation()
 {
-  startup_lorawan();
+  //startup_lorawan();
   uint8_t txBuffer[14];
   uint8_t bufferSize = 0;
   uint8_t port = 21;
   bufferSize = vbatt_bin(txBuffer, bufferSize); // get battery level
-  bufferSize = location_bin(txBuffer, bufferSize);
-  lorawan_send(port, txBuffer, bufferSize, 0, LORAWAN_DEFAULT_SF);
+  //bufferSize = location_bin(txBuffer, bufferSize);
+  //lorawan_send(port, txBuffer, bufferSize, 0, LORAWAN_DEFAULT_SF);
 }
 
 void sendStatus(int state, int gps)
 {
-  startup_lorawan();
+  //startup_lorawan();
   uint8_t txBuffer[14];
   uint8_t bufferSize = 0;
   uint8_t port = 1;
@@ -205,5 +102,5 @@ void sendStatus(int state, int gps)
   bufferSize++;
   txBuffer[bufferSize] = gps;
   bufferSize++;
-  lorawan_send(port, txBuffer, bufferSize, 0, STATUS_SF);
+  //lorawan_send(port, txBuffer, bufferSize, 0, STATUS_SF);
 }
