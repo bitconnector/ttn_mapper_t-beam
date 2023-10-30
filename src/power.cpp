c1dda94c1a1a48f350c845b2248f0f3978958715
Lukas <luk-krat@web.de>
2023-10-30 13:41:19 +0100

    correct pmu-config

--- src/power.cpp
+++ src/power.cpp
@@ -46,7 +46,8 @@ void startup_axp()
         }
     }
 
-    pinMode(AXP_IRQ, INPUT_PULLUP);
+    // pinMode(AXP_IRQ, INPUT_PULLUP);
+    pinMode(AXP_IRQ, INPUT);
     // attachInterrupt(digitalPinToInterrupt(AXP_IRQ), axp_interrupt, FALLING);
 }
 
@@ -102,96 +103,66 @@ void setup_axp()
     }
     else if (PMU->getChipModel() == XPOWERS_AXP2101)
     {
-        // m.2 interface
-        PMU->setPowerChannelVoltage(XPOWERS_DCDC3, 3300);
-        PMU->enablePowerOutput(XPOWERS_DCDC3);
+        // Unuse power channel
+        PMU->disablePowerOutput(XPOWERS_DCDC2);
+        PMU->disablePowerOutput(XPOWERS_DCDC3);
+        PMU->disablePowerOutput(XPOWERS_DCDC4);
+        PMU->disablePowerOutput(XPOWERS_DCDC5);
+        PMU->disablePowerOutput(XPOWERS_ALDO1);
+        PMU->disablePowerOutput(XPOWERS_ALDO4);
+        PMU->disablePowerOutput(XPOWERS_BLDO1);
+        PMU->disablePowerOutput(XPOWERS_BLDO2);
+        PMU->disablePowerOutput(XPOWERS_DLDO1);
+        PMU->disablePowerOutput(XPOWERS_DLDO2);
 
-        /**
-         * ALDO2 cannot be turned off.
-         * It is a necessary condition for sensor communication.
-         * It must be turned on to properly access the sensor and screen
-         * It is also responsible for the power supply of PCF8563
-         */
-        // PMU->setPowerChannelVoltage(XPOWERS_ALDO2, 3300);
-        // PMU->enablePowerOutput(XPOWERS_ALDO2);
+        // GNSS RTC PowerVDD 3300mV
+        PMU->setPowerChannelVoltage(XPOWERS_VBACKUP, 3300);
+        PMU->enablePowerOutput(XPOWERS_VBACKUP);
 
-        // 6-axis , magnetometer ,bme280 , oled screen power channel
-        // PMU->setPowerChannelVoltage(XPOWERS_ALDO1, 3300);
-        // PMU->enablePowerOutput(XPOWERS_ALDO1);
+        // ESP32 VDD 3300mV
+        //  ! No need to set, automatically open , Don't close it
+        //  PMU->setPowerChannelVoltage(XPOWERS_DCDC1, 3300);
+        //  PMU->setProtectedChannel(XPOWERS_DCDC1);
+        PMU->setProtectedChannel(XPOWERS_DCDC1);
 
-        // sdcard power channle
-        // PMU->setPowerChannelVoltage(XPOWERS_BLDO1, 3300);
-        // PMU->enablePowerOutput(XPOWERS_BLDO1);
-
-        // PMU->setPowerChannelVoltage(XPOWERS_DCDC4, 3300);
-        // PMU->enablePowerOutput(XPOWERS_DCDC4);
-
-        // lora radio power channel
-        PMU->setPowerChannelVoltage(XPOWERS_ALDO3, 3300);
-        PMU->enablePowerOutput(XPOWERS_ALDO3);
-
-        // m.2 interface
-        PMU->setPowerChannelVoltage(XPOWERS_DCDC3, 3300);
-        PMU->enablePowerOutput(XPOWERS_DCDC3);
-
-        /**
-         * ALDO2 cannot be turned off.
-         * It is a necessary condition for sensor communication.
-         * It must be turned on to properly access the sensor and screen
-         * It is also responsible for the power supply of PCF8563
-         */
+        // LoRa VDD 3300mV
         PMU->setPowerChannelVoltage(XPOWERS_ALDO2, 3300);
         PMU->enablePowerOutput(XPOWERS_ALDO2);
 
-        // 6-axis , magnetometer ,bme280 , oled screen power channel
-        PMU->setPowerChannelVoltage(XPOWERS_ALDO1, 3300);
-        PMU->enablePowerOutput(XPOWERS_ALDO1);
-
-        // sdcard power channle
-        PMU->setPowerChannelVoltage(XPOWERS_BLDO1, 3300);
-        PMU->enablePowerOutput(XPOWERS_BLDO1);
-
-        // not use channel
-        PMU->disablePowerOutput(XPOWERS_DCDC2); // not elicited
-        PMU->disablePowerOutput(XPOWERS_DCDC5); // not elicited
-        PMU->disablePowerOutput(XPOWERS_DLDO1); // Invalid power channel, it does not exist
-        PMU->disablePowerOutput(XPOWERS_DLDO2); // Invalid power channel, it does not exist
-        PMU->disablePowerOutput(XPOWERS_VBACKUP);
-
-        // disable all axp chip interrupt
-        PMU->disableIRQ(XPOWERS_AXP2101_ALL_IRQ);
-
-        // Set the constant current charging current of AXP2101, temporarily use 500mA by default
-        PMU->setChargerConstantCurr(XPOWERS_AXP2101_CHG_CUR_500MA);
-
-        // Set up the charging voltage
-        PMU->setChargeTargetVoltage(XPOWERS_AXP2101_CHG_VOL_4V2);
-
-        PMU->setPowerKeyPressOffTime(XPOWERS_AXP202_POWEROFF_10S);
+        // GNSS VDD 3300mV
+        PMU->setPowerChannelVoltage(XPOWERS_ALDO3, 3300);
+        PMU->enablePowerOutput(XPOWERS_ALDO3);
     }
-
-    PMU->clearIrqStatus();
-
-    // TBeam1.1 /T-Beam S3-Core has no external TS detection,
-    // it needs to be disabled, otherwise it will cause abnormal charging
+    PMU->enableSystemVoltageMeasure();
+    PMU->enableVbusVoltageMeasure();
+    PMU->enableBattVoltageMeasure();
+    // It is necessary to disable the detection function of the TS pin on the board
+    // without the battery temperature detection function, otherwise it will cause abnormal charging
     PMU->disableTSPinMeasure();
 
+    // Set the time of pressing the button to turn off
+    PMU->setPowerKeyPressOffTime(XPOWERS_POWEROFF_10S);
+
     uint64_t pmuIrqMask = 0;
+    uint64_t pmuIrqDis = 0;
 
     if (PMU->getChipModel() == XPOWERS_AXP192)
     {
+        pmuIrqDis = XPOWERS_AXP192_ALL_IRQ;
         // pmuIrqMask = XPOWERS_AXP192_VBUS_INSERT_IRQ | XPOWERS_AXP192_BAT_INSERT_IRQ | XPOWERS_AXP192_PKEY_SHORT_IRQ;
         pmuIrqMask = XPOWERS_AXP192_PKEY_SHORT_IRQ | XPOWERS_AXP192_PKEY_LONG_IRQ;
     }
     else if (PMU->getChipModel() == XPOWERS_AXP2101)
     {
-        // pmuIrqMask = XPOWERS_AXP2101_VBUS_INSERT_IRQ | XPOWERS_AXP2101_BAT_INSERT_IRQ | XPOWERS_AXP2101_PKEY_SHORT_IRQ;
-        pmuIrqMask = XPOWERS_AXP2101_PKEY_SHORT_IRQ | XPOWERS_AXP2101_PKEY_LONG_IRQ;
+        pmuIrqDis = XPOWERS_AXP2101_ALL_IRQ;
+        //pmuIrqMask |= XPOWERS_AXP2101_BAT_INSERT_IRQ | XPOWERS_AXP2101_BAT_REMOVE_IRQ;      // BATTERY
+        //pmuIrqMask |= XPOWERS_AXP2101_VBUS_INSERT_IRQ | XPOWERS_AXP2101_VBUS_REMOVE_IRQ;    // VBUS
+        pmuIrqMask |= XPOWERS_AXP2101_PKEY_SHORT_IRQ | XPOWERS_AXP2101_PKEY_LONG_IRQ;       // POWER KEY
+        //pmuIrqMask |= XPOWERS_AXP2101_BAT_CHG_DONE_IRQ | XPOWERS_AXP2101_BAT_CHG_START_IRQ; // CHARGE
     }
-
-    PMU->enableIRQ(pmuIrqMask);
-
+    PMU->disableIRQ(pmuIrqDis);
     PMU->clearIrqStatus();
+    PMU->enableIRQ(pmuIrqMask);
 
     axp_lora(1);
     axp_gps(1);
@@ -307,6 +278,9 @@ uint8_t axp_cause()
         Serial.println("isPekeyLongPress");
         ret = 2;
     }
+
+    PMU->clearIrqStatus();
+
     return ret;
 }
 
